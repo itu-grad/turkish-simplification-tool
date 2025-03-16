96688c1388a3d3f370b5a1cb364f4f4fd435f0a8
Berat Dalsuna <dalsunaberat@gmail.com>
2025-03-16 23:43:04 +0300

    new: add alternative text generate output

--- src/app/text-generation/Output.tsx
+++ src/app/text-generation/Output.tsx
@@ -1,41 +1,93 @@
+import { useState } from "react";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
 export default function TextGenerationOutput() {
+    const [currentIndex, setCurrentIndex] = useState<number>(0);
+    const [isFading, setIsFading] = useState<boolean>(false);
+
+    const alternatives = [
+        {
+            text: "Sehrin en önemli yerlerinden birisi de çöplükleridir. Çöplükler şehirler için gereklidir evet ama bu kadar önemli olduklarını hiç düşündünüz mü?",
+            words: ["Z - zürafa", "K - kendi", "G - gelişim", "Y - yolculuk"],
+            grammar: ["Geçmiş zaman", "Sıfat fiil", "Olumsuzluk eki", "Gelecek zaman"],
+        },
+        {
+            text: "İstanbul güzel şehir. İstanbul’un boy boy, renk renk resimleri yapılmıştır yıllar boyu.",
+            words: ["A - ağaç", "B - balık", "C - cam", "D - deniz"],
+            grammar: ["Şimdiki zaman", "Geniş zaman", "Emir kipi", "Dilek kipi"],
+        },
+        {
+            text: "Bir çöplük, bence bir şehir demektir. Martıların hayat kavgaları en çok çöplüklerde olur.",
+            words: ["E - elma", "F - fındık", "G - güneş", "H - hüzün"],
+            grammar: ["Zarf fiil", "İsim fiil", "Gelecek zaman", "Olumsuzluk eki"],
+        },
+    ];
+
+    const handleNext = () => {
+        setIsFading(true);
+        setTimeout(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % alternatives.length);
+            setIsFading(false);
+        }, 300);
+    };
+
+    const handlePrevious = () => {
+        setIsFading(true);
+        setTimeout(() => {
+            setCurrentIndex((prevIndex) =>
+                prevIndex === 0 ? alternatives.length - 1 : prevIndex - 1
+            );
+            setIsFading(false);
+        }, 300);
+    };
+
     return (
         <div className="p-8 min-w-[1200] bg-[#f5f5f5] rounded-xl shadow-lg flex flex-col space-y-6 mt-10 mb-10">
-            <div className="text-2xl font-medium text-[#1e1e1e]">Üretilen Metin</div>
-            <div className="p-6 bg-white rounded-md shadow-sm">
-                <p className="text-[#1e1e1e] text-justify">
-                    Şehrin en önemli yerlerinden birisi de çöplükleridir. Çöplükler şehirler için gereklidir
-                    evet ama bu kadar önemli olduklarını hiç düşündünüz mü? Bir büyük şehir çöplüğünü
-                    görünceye kadar bunu ben de bilmiyordum. Bir çöplük, bence bir şehir demektir.
-                    İstanbul güzel şehir. İstanbul’un boy boy, renk renk resimleri yapılmıştır yıllar boyu.
-                    Fotoğrafları çekilmiştir. Onunla ilgili şiirler yazılmıştır. Bunların birçoğunu gördüm,
-                    okudum. Ama ben size söylüyorum, hiçbir şey, hiç kimse İstanbul’u bana çöplükleri
-                    kadar anlatmadı. Neden çöplükleri böylesine yakından bilirim? Ben çöplük uzmanı
-                    değilim. Sebebini söyleyeyim. Bir, ben martıları çok severim… Hayır, daha çok onların
-                    hayatlarını merak ederim. Giderim, saatlerce onları seyrederim… Bir deniz üstünde, bir
-                    kayalıkta, bir çöplükte. Martıların hayat kavgaları en çok çöplüklerde olur. Çöplüklere
-                    ilk ilgim martılardan dolayıdır. Çöplüklere ikinci ilgim de bizim komşu Rüstem Çavuş’tan dolayıdır. Rüstem Çavuş uzun bıyıklı, canlı, şakacı, hayat dolu, sevgi dolu bir
-                    kişidir. Sivaslıdır. On yıldır da İstanbul’da çöpçüdür. Dört yıl önce çöpçü çavuşluğuna
-                    terfi etti.
-                </p>
-            </div>
+            <div className="flex flex-row gap-3">
+                <div className="max-w-[20] flex justify-center items-center">
+                    <FaArrowLeft
+                        className="text-gray-600 text-2xl cursor-pointer hover:text-blue-500"
+                        onClick={handlePrevious}
+                    />
+                </div>
+                <div className={`w-full transition-opacity duration-300 ${isFading ? "opacity-0" : "opacity-100"}`}>
+                    <div className="text-2xl font-medium text-[#1e1e1e] mb-5">Üretilen Metin</div>
+                    <div className="p-6 bg-white rounded-md shadow-sm">
+                        <p className="text-[#1e1e1e] text-justify">
+                            {alternatives[currentIndex].text}
+                        </p>
+                    </div>
 
-            <div className="text-xl font-medium text-[#1e1e1e] mt-6">Kullanılan Kelimeler</div>
-            <div className="grid grid-cols-4 gap-4 p-6 bg-white rounded-md shadow-sm">
-                <div className="text-[#1e1e1e]">Z - zürafa</div>
-                <div className="text-[#1e1e1e]">K - kendi</div>
-                <div className="text-[#1e1e1e]">G - gelişim</div>
-                <div className="text-[#1e1e1e]">Y - yolculuk</div>
-            </div>
+                    <div className="grid grid-cols-2 gap-6 mt-10">
+                        <div className="flex flex-col p-6 bg-white rounded-md shadow-sm">
+                            <p className="text-xl font-medium text-[#1e1e1e]">Kullanılan Kelimeler</p>
+                            <div className="overflow-y-scroll mt-4 max-h-32">
+                                {alternatives[currentIndex].words.map((word, index) => (
+                                    <div key={index} className="text-[#1e1e1e]">
+                                        {word}
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
 
-            <div className="text-xl font-medium text-[#1e1e1e] mt-6">Kullanılan Gramer Yapıları</div>
-            <div className="p-6 bg-white rounded-md shadow-sm">
-                <ul className="text-[#1e1e1e] text-left list-disc list-inside">
-                    <li>Geçmiş zaman</li>
-                    <li>Sıfat fiil</li>
-                    <li>Olumsuzluk eki</li>
-                    <li>Gelecek zaman</li>
-                </ul>
+                        <div className="flex flex-col p-6 bg-white rounded-md shadow-sm">
+                            <p className="text-xl font-medium text-[#1e1e1e]">Kullanılan Gramer Yapıları</p>
+                            <div className="overflow-y-scroll mt-4 max-h-32">
+                                <ul className="text-[#1e1e1e] text-left list-disc list-inside">
+                                    {alternatives[currentIndex].grammar.map((grammar, index) => (
+                                        <li key={index}>{grammar}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="max-w-[20] flex justify-center items-center">
+                    <FaArrowRight
+                        className="text-gray-600 text-2xl cursor-pointer hover:text-blue-500"
+                        onClick={handleNext}
+                    />
+                </div>
             </div>
         </div>
     );
