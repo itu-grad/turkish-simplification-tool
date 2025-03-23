a0a6500044e6ec3d2942a8ed3cb8f3158524a1ae
Berat Dalsuna <dalsunaberat@gmail.com>
2025-03-23 17:04:11 +0300

    new: add word grammer list component

--- src/app/text-analysis/output/Output.tsx
+++ src/app/text-analysis/output/Output.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import TableWithLevels from "@/components/TableWithLevels";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
@@ -9,11 +10,47 @@ export default function TextAnalysisOutputComponent() {
     const [isFading, setIsFading] = useState<boolean>(false);
     const router = useRouter();
     const [level, setLevel] = useState("C2");
-
+    const words = [
+        { text: "göz", level: "A2" },
+        { text: "köşk", level: "B2" },
+        { text: "taraf", level: "C2" },
+        { text: "temel", level: "A1" },
+        { text: "tavuk", level: "A1" },
+        { text: "peri", level: "A1" },
+        { text: "peri", level: "A1" },
+        { text: "peri", level: "A1" },
+        { text: "peri", level: "A1" },
+        { text: "peri", level: "A1" },
+        { text: "peri", level: "A1" },
+        { text: "peri", level: "A1" },
+        { text: "peri", level: "A1" },
+        { text: "peri", level: "A1" },
+        { text: "peri", level: "A1" },
+        { text: "peri", level: "A1" },
+        { text: "peri", level: "A1" },
+    ];
+    const rules = [
+        { text: "duyulan geçmiş zaman", level: "B2" },
+        { text: "görülen geçmiş zaman", level: "C2" },
+        { text: "sıfat fiil", level: "C2" },
+        { text: "olumsuzluk eki", level: "A2" },
+        { text: "gelecek zaman", level: "B1" },
+        { text: "gelecek zaman", level: "B1" },
+        { text: "gelecek zaman", level: "B1" },
+        { text: "gelecek zaman", level: "B1" },
+        { text: "gelecek zaman", level: "B1" },
+        { text: "gelecek zaman", level: "B1" },
+        { text: "gelecek zaman", level: "B1" },
+        { text: "gelecek zaman", level: "B1" },
+        { text: "gelecek zaman", level: "B1" },
+        { text: "gelecek zaman", level: "B1" },
+        { text: "gelecek zaman", level: "B1" },
+        { text: "gelecek zaman", level: "B1" },
+        { text: "ayrılma hal eki", level: "B1" }
+    ];
     return (
         <div className="p-8 min-w-[1200px] bg-[#f5f5f5] rounded-xl shadow-lg flex flex-col space-y-6 mt-10 mb-10">
 
-            {/* Left Section: Text & Dropdown */}
             <div className="flex space-x-6">
                 <div className="w-1/2 bg-white p-6 rounded-lg shadow-md flex flex-col">
                     <div
@@ -45,73 +82,9 @@ export default function TextAnalysisOutputComponent() {
                     </p>
                 </div>
 
-                {/* Right Section: Tables */}
                 <div className="flex max-h-[50vh] w-1/2 space-x-6 mt-20">
-                    {/* Word Table */}
-                    <div className="flex-1">
-                        <h3 className="font-semibold mb-2 text-[#1e1e1e]">Kelime</h3>
-                        <div className="flex-1 bg-white p-4 rounded-lg shadow-md">
-                            <div className="max-h-[40vh] overflow-y-scroll">
-                                {[
-                                    { word: "göz", level: "A2" },
-                                    { word: "köşk", level: "B2" },
-                                    { word: "taraf", level: "C2" },
-                                    { word: "temel", level: "A1" },
-                                    { word: "tavuk", level: "A1" },
-                                    { word: "peri", level: "A1" },
-                                    { word: "peri", level: "A1" },
-                                    { word: "peri", level: "A1" },
-                                    { word: "peri", level: "A1" },
-                                    { word: "peri", level: "A1" },
-                                    { word: "peri", level: "A1" },
-                                    { word: "peri", level: "A1" },
-                                    { word: "peri", level: "A1" },
-                                    { word: "peri", level: "A1" },
-                                    { word: "peri", level: "A1" },
-                                    { word: "peri", level: "A1" },
-                                    { word: "peri", level: "A1" },
-                                ].map((item, index) => (
-                                    <div key={index} className="flex justify-between py-1 mr-4">
-                                        <span className="text-gray-700">{item.word}</span>
-                                        <span className="text-[#1e1e1e]">{item.level}</span>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* Grammar Table */}
-                    <div className="flex-1">
-                        <h3 className="font-semibold mb-2 text-[#1e1e1e]">Gramer Yapısı</h3>
-                        <div className="flex-1 bg-white p-4 rounded-lg shadow-md">
-                            <div className="max-h-[40vh] overflow-y-scroll">
-                                {[
-                                    { rule: "duyulan geçmiş zaman", level: "B2" },
-                                    { rule: "görülen geçmiş zaman", level: "C2" },
-                                    { rule: "sıfat fiil", level: "C2" },
-                                    { rule: "olumsuzluk eki", level: "A2" },
-                                    { rule: "gelecek zaman", level: "B1" },
-                                    { rule: "gelecek zaman", level: "B1" },
-                                    { rule: "gelecek zaman", level: "B1" },
-                                    { rule: "gelecek zaman", level: "B1" },
-                                    { rule: "gelecek zaman", level: "B1" },
-                                    { rule: "gelecek zaman", level: "B1" },
-                                    { rule: "gelecek zaman", level: "B1" },
-                                    { rule: "gelecek zaman", level: "B1" },
-                                    { rule: "gelecek zaman", level: "B1" },
-                                    { rule: "gelecek zaman", level: "B1" },
-                                    { rule: "gelecek zaman", level: "B1" },
-                                    { rule: "gelecek zaman", level: "B1" },
-                                    { rule: "ayrılma hal eki", level: "B1" },
-                                ].map((item, index) => (
-                                    <div key={index} className="flex justify-between py-1 mr-4">
-                                        <span className="text-gray-700">{item.rule}</span>
-                                        <span className="text-[#1e1e1e]">{item.level}</span>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
+                    <TableWithLevels title={"Kelimeler"} levelList={words} width={0} />
+                    <TableWithLevels title={"Gramer Yapıları"} levelList={rules} width={0} />
                 </div>
             </div>
 
