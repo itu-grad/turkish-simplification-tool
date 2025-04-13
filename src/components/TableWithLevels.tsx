600f941b2ba1c740b4a8917017395aa868e43a3e
Berat Dalsuna <dalsunaberat@gmail.com>
2025-04-13 19:20:34 +0300

    new: add loading for levels

--- src/components/TableWithLevels.tsx
+++ src/components/TableWithLevels.tsx
@@ -5,21 +5,27 @@ interface Props {
 }
 
 export default function TableWithLevels({ title, levelList, width }: Props) {
+    const isEmpty = Object.keys(levelList).length === 0;
+
     return (
         <div className="flex-1">
             <h3 className="font-semibold mb-2 text-header">{title}</h3>
             <div className="flex-1 bg-white p-4 rounded-lg shadow-md">
                 <div className="max-h-[40vh] overflow-y-scroll">
-                    {Object.entries(levelList).map((item, index) => {
-                        if (item[1]) {
-                            return (
+                    {isEmpty ? (
+                        <div className="flex items-center justify-center">
+                            <div className="animate-spin rounded-full h-5 w-5 border-b-2 border-subheader"></div>
+                        </div>
+                    ) : (
+                        Object.entries(levelList).map(([word, level], index) =>
+                            level ? (
                                 <div key={index} className="flex justify-between py-1 mr-4">
-                                    <span className="text-paragraph">{item[0]}</span>
-                                    <span className="text-header">{item[1]}</span>
+                                    <span className="text-paragraph">{word}</span>
+                                    <span className="text-header">{level}</span>
                                 </div>
-                            );
-                        }
-                    })}
+                            ) : null
+                        )
+                    )}
                 </div>
             </div>
         </div>
